38646894b50e2ba3be61c3298119c7654cc8b34c
paulorsouza <paulor1809@gmail.com>
2018-11-26 22:48:16 +0000

    Implements maxchar function

---
+++ lib/max_char.ex
@@ -0,0 +1,37 @@
+defmodule MaxChar do
+  @moduledoc """
+  Given a string, return the character that is most commonly used in the string
+  """
+
+  @doc """
+  Max char
+
+  ## Examples
+
+      iex> MaxChar.max_char("abcccccccd")
+      "c"
+      iex> MaxChar.max_char("apple 12311111")
+      "1"
+  """
+  def max_char(string) do
+    mapped_string = map_string(string)
+    {key, _} =Enum.max_by(mapped_string, fn map ->
+      {_, value} = map
+      value
+    end)
+    key
+  end
+
+  defp map_string(string) do
+    string
+    |> String.graphemes()
+    |> Enum.reduce(%{}, &map_string/2)
+  end
+
+  defp map_string(char, acc) do
+    case Map.get(acc, char) do
+      nil -> Map.put_new(acc, char, 1)
+      value -> Map.put(acc, char, value + 1)
+    end
+  end
+end
---
+++ test/max_char_test.exs
@@ -0,0 +1,16 @@
+defmodule MaxCharTest do
+  use ExUnit.Case
+  import MaxChar
+
+  doctest MaxChar
+
+  test "Finds the most frequently used char" do
+    assert max_char("a") == "a"
+    assert max_char("absfdsafadaasfasaaaaaaaa") == "a"
+  end
+
+  test "Works with numbers in the string" do
+    assert max_char("ab1111cd1112111d") == "1"
+  end
+
+end
