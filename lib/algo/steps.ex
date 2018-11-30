b8a7795bda234d63bfd0b11f979d4ae29ed81b32
paulorsouza <paulor1809@gmail.com>
2018-11-30 21:54:30 +0000

    Implements steps logic

---
+++ lib/algo/steps.ex
@@ -0,0 +1,47 @@
+defmodule Algo.Steps do
+  @moduledoc """
+  Write a function that accepts a positive number N.
+  The function should console log a step shape
+  with N levels using the # character.  Make sure the
+  step has spaces on the right hand side!
+  """
+
+  @doc """
+  Print steps with fucntions
+  """
+  def print_steps(n) do
+    print_steps(n, 1, "")
+  end
+
+  defp print_steps(n, row, _) when n < row, do: nil
+
+  defp print_steps(n, row, line) do
+    if(String.length(line) == n) do
+      IO.puts line
+      print_steps(n, row + 1, "")
+    else
+      if(String.length(line) >= row) do
+        print_steps(n, row, line <> " ")
+      else
+        print_steps(n, row, line <> "#")
+      end
+    end
+  end
+
+  @doc """
+  Print steps with comprehensions
+  """
+  def print_lines(steps) do
+    rows = 1..steps
+    columns = 1..steps
+
+    for column <- columns, row <- rows do
+      if (row <= column) do
+         IO.write "#"
+       else
+         IO.write " "
+      end
+      if (row == steps and column != steps), do: IO.puts ""
+    end
+  end
+end
--- test/algo/steps_test.exs
+++ test/algo/steps_test.exs
@@ -1,3 +1,32 @@
 defmodule Algo.StepsTests do
+  use ExUnit.Case
+  import Algo.Steps
+  import ExUnit.CaptureIO
+
+  doctest Algo.Steps
+
+  test "should output 5 lines with # progressively with print_steps" do
+    assert assert capture_io(fn ->
+      print_steps(5)
+    end) == "#    \n##   \n###  \n#### \n#####\n"
+  end
+
+  test "should output 3 lines with # progressively with print_steps" do
+    assert assert capture_io(fn ->
+      print_steps(3)
+    end) == "#  \n## \n###\n"
+  end
+
+  test "should output 5 lines with # progressively" do
+    assert assert capture_io(fn ->
+      print_lines(5)
+    end) == "#    \n##   \n###  \n#### \n#####"
+  end
+
+  test "should output 3 lines with # progressively" do
+    assert assert capture_io(fn ->
+      print_lines(3)
+    end) == "#  \n## \n###"
+  end
 
 end
